ce19d69ced9376d42adc84ea1ec21b4b25e5610b
Seth Etter <sethetter@gmail.com>
2014-11-15 13:07:09 -0600

    Require landlord policy, not used yet though

---
+++ api/policies/requireLandlord.js
@@ -0,0 +1,20 @@
+/**
+ * requireLandlord
+ *
+ * @module      :: Policy
+ * @description :: Check that logged in user is a landlord
+ * @docs        :: http://sailsjs.org/#!documentation/policies
+ *
+ */
+module.exports = function(req, res, next) {
+
+  // User is allowed, proceed to the next policy,
+  // or if this is the last policy, the controller
+  if (req.session.authenticated || req.session.user.type === 'landlord') {
+    return next();
+  }
+
+  // User is not allowed
+  // (default res.forbidden() behavior can be overridden in `config/403.js`)
+  return res.forbidden('You are not permitted to perform this action.');
+};
