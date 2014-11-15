7efe6742f06eaf55509179e0010f3a91e045a7c7
Christian Taylor <christian@lifeboatcreative.com>
2014-11-15 10:53:43 -0600

    Login form!!!

---
+++ assets/js/modules.js
@@ -0,0 +1,9 @@
+var RK_MODULES = window.RK_MODULES || {};
+
+RK_MODULES.flexslider = function() {
+	$('.slider').flexslider({
+		animation: 'slide', 
+		directionNav: false,
+	    controlNav: false
+	});
+};
