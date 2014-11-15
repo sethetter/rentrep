7dd236edb1a7a9f14d5830b3b48fe786e8c9a6ad
Seth Etter <sethetter@gmail.com>
2014-11-15 13:08:15 -0600

    Require tenant policy, plus update to landlord one

--- api/policies/requireTenant.js
+++ api/policies/requireTenant.js
@@ -1,8 +1,8 @@
 /**
- * requireLandlord
+ * requireTenant
  *
  * @module      :: Policy
- * @description :: Check that logged in user is a landlord
+ * @description :: Check that logged in user is a tenant
  * @docs        :: http://sailsjs.org/#!documentation/policies
  *
  */
@@ -10,11 +10,11 @@ module.exports = function(req, res, next) {
 
   // User is allowed, proceed to the next policy,
   // or if this is the last policy, the controller
-  if (req.session.authenticated || req.session.user.type === 'landlord') {
+  if (req.session.authenticated || req.session.user.type === 'tenant') {
     return next();
   }
 
   // User is not allowed
   // (default res.forbidden() behavior can be overridden in `config/403.js`)
-  return res.forbidden('You must be a landlord to perform this action.');
+  return res.forbidden('You must be a tenant to perform this action.');
 };
