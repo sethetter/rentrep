be56c56f1e0b0774e2cf26fcd42bcd8182a6d3eb
Seth Etter <sethetter@gmail.com>
2014-11-16 11:44:23 -0600

    Rating calculator, placeholder

---
+++ api/services/RatingCalulator.js
@@ -0,0 +1,29 @@
+/**
+ * RatingCalculator
+ *
+ * Calculate ratings for landlords, tenants, and properties
+ */
+module.exports = {
+
+  /**
+   * landlord
+   *
+   * TODO: What's the algorithm?
+   */
+  landlord: function(ratings) {},
+
+  /**
+   * tenant
+   *
+   * TODO: What's the algorithm?
+   */
+  tenant: function(ratings) {},
+
+  /**
+   * property
+   *
+   * TODO: What's the algorithm?
+   */
+  property: function(ratings) {},
+
+};
